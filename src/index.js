7e1602e19683238b7fafd57ad6ab47ff257fe332
Eddie Soller <esoller@apexclearing.com>
2022-02-20 21:16:29 -0800

    add auto sizing scoreboard text

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ window.addEventListener('load', () => {
 })
 
 window.addEventListener('resize', () => {
-    adjustGrid();
+    adjustView();
 })
 
 document.addEventListener('keypress', () => {
     container.appendChild(createScoreboard());
-    adjustGrid();
+    adjustView();
 });
 
 function createHeader() {
@@ -142,6 +142,12 @@ function createAt() {
     return span;
 }
 
+function adjustView() {
+    adjustGrid();
+    adjustTime();
+    adjustScoreFontSize();
+}
+
 function adjustGrid() {
     let factors = factorize(count);
     let optimalColumns = {
@@ -162,16 +168,87 @@ function adjustGrid() {
     container.style.gridTemplateColumns = AUTO.repeat(optimalColumns.value);
 }
 
+function adjustTime() {
+    const scoreboard = document.getElementsByClassName('scoreboard')[0];
+    if (!scoreboard) {
+        return;
+    }
+
+    const time = document.getElementsByClassName('time')[0];
+    let fontSize = Math.min(scoreboard.offsetHeight, 16);
+    setTimeFontSize(fontSize);
+
+    while (timeTooBig(time, scoreboard) && fontSize > 1) {
+        fontSize--;
+        setTimeFontSize(fontSize);
+    }
+}
+
+function timeTooBig(time, scoreboard) {
+    return scoreboard.getBoundingClientRect().bottom - time.getBoundingClientRect().bottom < 5;
+}
+
+function setTimeFontSize(fontSize) {
+    const times = document.getElementsByClassName('time');
+    for (i = 0; i < times.length; i++) {
+        times[i].style.fontSize = fontSize + 'px';
+    }
+}
+
+function adjustScoreFontSize() {
+    const scoreboards = document.getElementsByClassName('scoreboard');
+    if (!scoreboards[0]) {
+        return;
+    }
+    const scores = document.getElementsByClassName('score');
+    const width = scoreboards[0].offsetWidth;
+    const height = scoreboards[0].offsetHeight;
+    let fontSize = Math.min(width, height, 50);
+    setScoreFontSize(fontSize);
+
+    for (i = 0; i < scores.length; i++) {
+        if (fontSize < 10) {
+            break;
+        }
+        if (scoreTooBig(scores[i], scoreboards[i])) {
+            fontSize--;
+            setScoreFontSize(fontSize);
+            i = 0;
+        }
+    }
+}
+
+function scoreTooBig(score, scoreboard) {
+    if (scoreboard.offsetHeight - score.offsetHeight < 10) {
+        return true;
+    }
+    const scoreSpan = score.firstChild.getBoundingClientRect();
+    const time = scoreboard.firstChild.getBoundingClientRect();
+    if (scoreSpan.top < time.bottom && scoreSpan.left < time.right) {
+        return true;
+    }
+    return false;
+}
+
+function setScoreFontSize(fontSize) {
+    const spans = document.getElementsByClassName('scoreSpan');
+    for (i = 0; i < spans.length; i++) {
+        spans[i].style.fontSize = fontSize + 'px';
+    }
+}
+
 function seedGames() {
     let games = getNbaData(yyyyMmDd());
     if (!games) {
         games = getNbaData("20220215");
     }
-    console.log('games', games);
     for (i = 0; i < games.length; i++) {
-        container.appendChild(createNbaScoreBoard(games[i]));
+        const nbaScoreBoard = createNbaScoreBoard(games[i]);
+        if (nbaScoreBoard) {
+            container.appendChild(nbaScoreBoard);
+        }
     }
-    adjustGrid();
+    adjustView();
 }
 
 function factorize(num) {
